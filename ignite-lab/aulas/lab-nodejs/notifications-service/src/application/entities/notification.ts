486ed9c43048ed9d4d1a63644132f959f11cc672
ALLAN VERGILIO FELIZZETI <alllanvergilio@gmail.com>
2022-12-14 22:40:00 -0300

    Projeto de Nodejs

---
+++ ignite-lab/aulas/lab-nodejs/notifications-service/src/application/entities/notification.ts
@@ -0,0 +1,25 @@
+
+export interface NotificationProps {
+    recipientId: string;
+    content: string;
+    category: string;
+    readAt?: Date | null;
+    createsAt: Date;
+}
+
+export class Notification {
+    private props: NotificationProps;
+
+    constructor(props: NotificationProps) {
+        this.props = props;
+    }
+
+    public set content(content: string) {
+        this.props.content = content; 
+    }
+
+    public get content(): string {
+        return this.props.content;
+    }
+
+}
