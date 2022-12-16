f82ca39c276b6c8b378704e023f1f9361154e5da
ALLAN VERGILIO FELIZZETI <alllanvergilio@gmail.com>
2022-12-15 21:26:54 -0300

    Ajeitando pastas e Criando Teste Unitarios

--- ignite-lab/aulas/lab-nodejs/notifications-service/src/application/entities/notification.ts
+++ ignite-lab/aulas/lab-nodejs/notifications-service/src/application/entities/notification.ts
@@ -1,25 +1,57 @@
+import { Replace } from "src/helpers/Replace";
+import { Content } from "./content";
 
 export interface NotificationProps {
     recipientId: string;
-    content: string;
+    content: Content;
     category: string;
     readAt?: Date | null;
-    createsAt: Date;
+    createAt: Date;
 }
 
 export class Notification {
     private props: NotificationProps;
 
-    constructor(props: NotificationProps) {
-        this.props = props;
+    constructor(props: Replace<NotificationProps, { createAt?: Date }>) {
+        this.props = {
+            ...props,
+            createAt: props.createAt ?? new Date(),
+        };
     }
 
-    public set content(content: string) {
+    public set recipientId(recipientId: string) {
+        this.props.recipientId = recipientId; 
+    }
+
+    public getrecipientIdt(): string {
+        return this.props.recipientId;
+    }
+
+    public set content(content: Content) {
         this.props.content = content; 
     }
 
-    public get content(): string {
+    public get content(): Content {
         return this.props.content;
     }
+    
+    public set category(category: string) {
+        this.props.category = category; 
+    }
 
+    public get categoryt(): string {
+        return this.props.category;
+    }
+
+    public set readAt(readAt: Date | null | undefined) {
+        this.props.readAt = readAt; 
+    }
+
+    public get readAt(): Date | null | undefined {
+        return this.props.readAt;
+    }
+
+    public get createdAt(): Date {
+        return this.props.createAt;
+    }
 }
